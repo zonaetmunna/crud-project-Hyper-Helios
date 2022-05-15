38f14e97015bcbb4cf364fd29ee3194468b7b1b2
zonaetmunna <zonaetmonna@gmail.com>
2022-05-15 16:09:45 +0600

    update

--- src/view/pages/contacData/EditContact.js
+++ src/view/pages/contacData/EditContact.js
@@ -11,15 +11,9 @@ const EditContact = () => {
      //for matching id 
      const contact=useSelector(state=>state.contacts.find(s => s.id ===contactId));
 
-     const [data,setData]=useState({});
+     const [name,setName]=useState(contact.name);
+     const [number,setNumber]=useState(contact.number);
 
-     const handleChange=(e)=>{
-          const field=e.target.name;
-          const value=e.target.value;
-          let newData={...data};
-          newData[field]=value;
-          setData(newData);
-     }
 
      const dispatch=useDispatch();
      const navigate=useNavigate();
@@ -27,12 +21,14 @@ const EditContact = () => {
      // handle contact update
      const handleUpdate=(e)=>{
           e.preventDefault();
-          if(data.name&&data.number){
+          if(name&&number){
                dispatch(
                     updateContact({
+                         
                          id:contactId,
-                         name:data.name,
-                         number:data.number})
+                         name:name,
+                         number:number
+                    })
                );
           }
           navigate('/contactData');
@@ -52,7 +48,7 @@ const EditContact = () => {
                               defaultValue={contact.name}  
                               id=""
                               placeholder='your name'
-                              onChange={handleChange}
+                              onChange={(e)=>setName(e.target.value)}
                               required    
                          />
                          <br />
@@ -62,7 +58,7 @@ const EditContact = () => {
                               defaultValue={contact.number} 
                               id="" 
                               placeholder='your number'
-                              onChange={handleChange}
+                              onChange={(e)=>setNumber(e.target.value)}
                               // pattern = "^(?:\\+88|88)?(01[3-9]\\d{8})$"
                               required
                          />
